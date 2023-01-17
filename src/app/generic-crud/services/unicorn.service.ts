c4f3bcab096517e75d2de7b2b39445803daa1482
Hall, Dylan <Dylan.Hall@edisonfinancial.ca>
2023-01-17 00:04:00 -0500

    upgraded to use

--- src/app/generic-crud/services/unicorn.service.ts
+++ src/app/generic-crud/services/unicorn.service.ts
@@ -1,17 +1,17 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
-import { Unicorn } from '../models/unicorn.model';
-import { ResourceService } from './resource.service';
+import { Unicorn } from "../models/unicorn.model";
+import { ResourceService } from "./resource.service";
 
-@Injectable({ providedIn: 'root' })
+@Injectable({ providedIn: "root" })
 export class UnicornsService extends ResourceService<Unicorn> {
   constructor(private http: HttpClient) {
     super(
       http,
       Unicorn,
       // SEE: https://crudcrud.com/
-      `https://crudcrud.com/api/9d5d443ffce9481581e177e7cb396ac9/unicorns`
+      `https://localhost:44382/api/apples`
     );
   }
 }
